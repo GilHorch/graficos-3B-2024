2b5d912a2dd093daa2012cdb223011c4e63f7853
Prof. Gilmar Horchulhak <108031801+GilHorch@users.noreply.github.com>
2024-09-25 11:08:26 -0300

    1

--- InformacoeGlobais.js
+++ InformacoeGlobais.js
@@ -8,6 +8,15 @@ async function visualizarInformacoesGlobais(params) {
     //contante que armazena uma repossta  await=espera ////fetch=faz a requisição
     const res = await fetch(url);
     const dados = await res.json();//espera as reposta erem convertidas em JSON
-console.log(dados);
+console.log(dados);//visualizar as informações no cosole
+const paragrafo=document.createElement('p');//criar um elemento de parágrafo
+
+paragrafo.classList.add('graficos-container__texto');//adiciona uma clase do CSS ao parégrafo
+
+/*insere o texto "Você sabia que o mundo tem "+total_pessoas_mundo */
+paragrafo.innerHTML=`Você sabia que o mundo tem ${dados.total_pessoas_mundo}`
+
+
+
 }
 visualizarInformacoesGlobais();//chama a função 
