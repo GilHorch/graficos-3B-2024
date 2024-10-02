e2a4d2727b152387074348d365cf730f7b7aaf03
Prof. Gilmar Horchulhak <108031801+GilHorch@users.noreply.github.com>
2024-10-02 12:12:36 -0300

    1

--- InformacoeGlobais.js
+++ InformacoeGlobais.js
@@ -1,4 +1,4 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json';
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
 console.log(url);
 /*função assincrona, espera até que toda a 
