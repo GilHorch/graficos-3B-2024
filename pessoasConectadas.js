308ba88cfde53b2b7fe1e31da9860aa28aa4aaee
Prof. Gilmar Horchulhak <108031801+GilHorch@users.noreply.github.com>
2024-10-02 11:56:33 -0300

    1

--- pessoasConectadas.js
+++ pessoasConectadas.js
@@ -1,7 +1,13 @@
+/*função assincrona, espera até que toda a 
+requisição seja executada, ou seja,
+todos os dados sejam enviadoss*/
 async function quantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero_usuarios.json'
-    const res = await fetch(url)
-    const dados = await res.json()
-      
-      console.log(dados)
-  }
+    //contante que armazena uma repossta  await=espera ////fetch=faz a requisição
+    const res = await fetch(url);
+    //espera as reposta serem convertidas em JSON
+    const dados = await res.json();
+
+    console.log(dados);
+}
+quantidadeUsuarios();//chama a função
