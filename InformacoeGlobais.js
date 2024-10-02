87ccf8ff9ba652a7e1451dc7f83391f8de800999
Prof. Gilmar Horchulhak <108031801+GilHorch@users.noreply.github.com>
2024-10-02 11:24:28 -0300

    1

--- InformacoeGlobais.js
+++ InformacoeGlobais.js
@@ -13,7 +13,8 @@ const pessoasNoMundo=(dados.total_pessoas_mundo/1e9)
 const horas= parseInt(dados.tempo_medio)//cria uma variável que utiliza apenas a parte inteira do numero de horas
 //cria ua variável que utiliza apenas os minuto das horas faz o arredondamento com a função "Math.round()"
 const minutos= Math.round((dados.tempo_medio-horas)*60); 
-
+//cria uma variável que calcula o percentual de pessoas conectadas no mundo e fixa para exibir com duas casas decimais
+const porcentagem =((pessoasConectadas/pessoasNoMundo)*100).toFixed(2);
 
 console.log(dados);//visualizar as informações no cosole
 const paragrafo=document.createElement('p');//criar um elemento de parágrafo
@@ -23,7 +24,12 @@ paragrafo.classList.add('graficos-container__texto');//adiciona uma clase do CSS
 /*insere o texto "Você sabia que o mundo tem "+total_pessoas_mundo ...*/
 paragrafo.innerHTML=`Você sabia que o mundo tem <span> ${pessoasNoMundo}bilhões </span> de pesoas
 e que aproximadamente <span>${pessoasConectadas} bilhões </span>  estão conectadas em alguma
-rede social e passam em média <span>${horas} horas</span> e   <span> ${minutos} minutos</span> conectadas.`
+rede social e passam em média <span>${horas} horas</span> e <span> ${minutos} minutos</span> conectadas.<br>
+Isso significa que aproximadamente <span>${porcentagem}%</span> de pessoas estão conctadas em alguma rede social
+
+
+
+`
 //cria a variável "container", seleciona o ID "graficos-container" na section do HTML 
 const container= document.getElementById('graficos-container');
 container.appendChild(paragrafo);//insere o paragrafo dentro do "container"
