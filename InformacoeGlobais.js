329ad7bee19d29daf2f6304d1114679e8e80bdea
Prof. Gilmar Horchulhak <108031801+GilHorch@users.noreply.github.com>
2024-10-02 11:07:33 -0300

    1

--- InformacoeGlobais.js
+++ InformacoeGlobais.js
@@ -8,15 +8,22 @@ async function visualizarInformacoesGlobais(params) {
     //contante que armazena uma repossta  await=espera ////fetch=faz a requisição
     const res = await fetch(url);
     const dados = await res.json();//espera as reposta erem convertidas em JSON
+const pesoasConectadas=(dados.total_pessoas_conectadas/1e9);//cria uma variável, pega os "dados.total_pessoas_conectadas" e divide um bilhão
+const pessoasNoMundo=(dados.total_pessoas_mundo/1e9)
+const horas= parseInt(dados.tempo_medio)//cria uma variável que utiliza apenas a parte inteira do numero de horas
+//cria ua variável que utiliza apenas os minuto das horas faz o arredondamento com a função "Math.round()"
+const minutos= Math.round((dados.tempo_medio-horas)*60); 
+
+
 console.log(dados);//visualizar as informações no cosole
 const paragrafo=document.createElement('p');//criar um elemento de parágrafo
 
 paragrafo.classList.add('graficos-container__texto');//adiciona uma clase do CSS ao parégrafo
 
 /*insere o texto "Você sabia que o mundo tem "+total_pessoas_mundo ...*/
-paragrafo.innerHTML=`Você sabia que o mundo tem <span> ${dados.total_pessoas_mundo}</span> de pesoas
-e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma
-rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+paragrafo.innerHTML=`Você sabia que o mundo tem <span> ${pessoasNoMundo}bilhões </span> de pesoas
+e que aproximadamente <span>${pesoasConectadas} bilhões </span>  estão conectadas em alguma
+rede social e passam em média <span>${horas}</span> horas e ${minutos} minutos conectadas.`
 //cria a variável "container", seleciona o ID "graficos-container" na section do HTML 
 const container= document.getElementById('graficos-container');
 container.appendChild(paragrafo);//insere o paragrafo dentro do "container"
