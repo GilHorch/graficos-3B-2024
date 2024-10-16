7d7a5c7117c74f986c1621e25dc8d536ac11bbdc
Prof. Gilmar Horchulhak <108031801+GilHorch@users.noreply.github.com>
2024-10-16 11:16:26 -0300

    1

--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -2,7 +2,22 @@ async function quantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
-      
-      console.log(dados)
-  }
+
+    console.log(dados)
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadedeUsuarios = Object.values(dados)
+
+    const data = [
+        {
+            x: nomeDasRedes,
+            y: quantidadeUsuarios,
+            type: 'bar'//grafico de barras
+        }
+    ]
+const grafico= document.createElement('div')//cria uma div
+grafico.className='grafico'//cria aclasse para a div
+//insere a div  e a classe na tag onte tem o id = "graficos-container"
+document.getElementById('graficos-container').appendChild('grafico')  
+Plotly.newPlot(grafico)
+}
 quantidadeUsuarios()
