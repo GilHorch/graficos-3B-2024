7f5ba581b316a86137f0d11202f5c9320809218c
Prof. Gilmar Horchulhak <108031801+GilHorch@users.noreply.github.com>
2024-10-23 11:36:53 -0300

    1

--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -1,4 +1,4 @@
-import { getCSS } from "./common"
+import { getCSS } from"common.js" //"./common"
 
 async function quantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
