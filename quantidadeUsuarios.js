d413887ccf7fb6fc1af819cd0e8bbb420dd70cbc
Prof. Gilmar Horchulhak <108031801+GilHorch@users.noreply.github.com>
2024-10-23 11:22:01 -0300

    1

--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -1,28 +1,32 @@
+import { getCSS } from "./common"
+
 async function quantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
-
     console.log(dados)
     const nomeDasRedes = Object.keys(dados)
     const quantidadeDeUsuarios = Object.values(dados)
-
     const data = [
         {
             x: nomeDasRedes,
             y: quantidadeDeUsuarios,
             type:'bar',//grafico de barras
             marker:{
-                color:getComputedStyle(document.body).getPropertyValue('--primary-color')
-
+                color:getCSS('--primary-color')
+                //getComputedStyle(document.body).getPropertyValue('--primary-color')
             }
         }
     ]
 
+    const layout={
+        //cor de fundo do gráfico
+        plot_bgcolor:getCSS('--bg-color')
+    }
     const grafico = document.createElement('div')//cria uma div
     grafico.className = 'grafico'//cria aclasse para a div
     //insere a div  e a classe na tag onte tem o id = "graficos-container"
     document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data)
+    Plotly.newPlot(grafico, data,layout)
 }
 quantidadeUsuarios()
